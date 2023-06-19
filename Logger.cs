41a6c6def044ef77eeaf8dc7de7e63571f314235
Kain (Pseudonym) <88363187+EKQRCalamity@users.noreply.github.com>
2023-06-19 21:50:38 +0200

    Updated Logger
    Multiple Improvements.
     - Unloading of Console (Free console)
     - Free Console if one is already allocated
     - Multiple other small improvements

--- Logger.cs
+++ Logger.cs
@@ -1,10 +1,15 @@
 using Microsoft.Win32.SafeHandles;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Base
+namespace AliceCradles2
 {
     enum LogType
     {
@@ -19,6 +24,7 @@ namespace Base
     internal class Logger : MonoBehaviour
     {
         internal static bool isAlloc = false;
+        internal static bool isShown = true;
 
         [DllImport("kernel32.dll",
             EntryPoint = "GetStdHandle",
@@ -26,16 +32,45 @@ namespace Base
             CharSet = CharSet.Auto,
             CallingConvention = CallingConvention.StdCall)]
         private static extern IntPtr GetStdHandle(int nStdHandle);
-        
+
         [DllImport("kernel32.dll",
             EntryPoint = "AllocConsole",
             SetLastError = true,
             CharSet = CharSet.Auto,
             CallingConvention = CallingConvention.StdCall)]
         private static extern int AllocConsole();
-        
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "FreeConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int FreeConsole();
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetConsoleWindow",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "ShowWindow",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
         private const int STD_OUTPUT_HANDLE = -11;
-        
+
+        private const int SW_HIDE = 0;
+        private const int SW_NORMAL = 1;
+        private const int SW_SHOWMINIMZED = 2;
+        private const int SW_SHOWMAXIMIZED = 3;
+        private const int SW_SHOWNOACTIVE = 4;
+        private const int SW_SHOW = 5;
+        private const int SW_MINIMIZE = 6;
+
         void OnEnable() { Application.logMessageReceived += LogF; }
         void OnDisable() { Application.logMessageReceived -= LogF; }
 
@@ -43,7 +78,8 @@ namespace Base
         {
             if (isAlloc)
             {
-                switch (type) {
+                switch (type)
+                {
                     case UnityEngine.LogType.Error:
                         Console.ForegroundColor = ConsoleColor.DarkRed;
                         condition = $"[Unity Error] - {condition}";
@@ -94,21 +130,110 @@ namespace Base
                         Console.ForegroundColor = ConsoleColor.Magenta;
                         break;
                 }
-                Console.WriteLine(print.ToString());
+                Console.WriteLine($"[{Assembly.GetCallingAssembly().GetName().Name}] - [Logger - {DateTime.Now.ToString("HH:mm:ss")}]: {print.ToString()}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        public static void LogNoNewLine(System.Object print, LogType logtype = LogType.Default)
+        {
+            if (isAlloc)
+            {
+                switch (logtype)
+                {
+                    case LogType.Default:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    case LogType.Warning:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case LogType.Error:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case LogType.Success:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case LogType.Blue:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case LogType.Magenta:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                }
+                Console.Write($"[{Assembly.GetCallingAssembly().GetName().Name}] - [Logger - {DateTime.Now.ToString("HH:mm:ss")}]: {print.ToString()}");
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
 
+        public static void LogReplaceLine(System.Object print, LogType logtype = LogType.Default)
+        {
+            if (isAlloc)
+            {
+                switch (logtype)
+                {
+                    case LogType.Default:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    case LogType.Warning:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case LogType.Error:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case LogType.Success:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case LogType.Blue:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case LogType.Magenta:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                }
+                int currentLineCursor = Console.CursorTop;
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currentLineCursor);
+                Console.Write($"[{Assembly.GetCallingAssembly().GetName().Name}] - [Logger - {DateTime.Now.ToString("HH:mm:ss")}]: {print.ToString()}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        internal static void ToggleConsole()
+        {
+            IntPtr handle = GetConsoleWindow();
+            if (isShown)
+                ShowWindow(handle, SW_HIDE);
+            else
+                ShowWindow(handle, SW_SHOWNOACTIVE);
+            isShown = !isShown;
+
+        }
+
         internal static void SetupConsole()
         {
-            AllocConsole();
-            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
-            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
-            StreamWriter standardOutput = new StreamWriter(fileStream);
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
-            isAlloc = true;
+            if (GetConsoleWindow() != IntPtr.Zero)
+                FreeConsole();
+            if (GetConsoleWindow() == IntPtr.Zero)
+            {
+                AllocConsole();
+                IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+                SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+                FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+                StreamWriter standardOutput = new StreamWriter(fileStream);
+                standardOutput.AutoFlush = true;
+                Console.SetOut(standardOutput);
+                isAlloc = true;
+            }
+        }
+
+        internal static void UnloadConsole()
+        {
+            if (GetConsoleWindow() != IntPtr.Zero)
+            {
+                FreeConsole();
+                isAlloc = false;
+                Console.SetOut(Console.Out);
+            }
         }
     }
 }
